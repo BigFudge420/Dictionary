daeaaf9624bfe518a15750b2b3ddb5af3d7a2595
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-10-08 18:52:18 +0530

    Fetch data and console log; debounce search

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import useFetchDefinition from './components/useFetchDefinition'
+import debounce from '../../js-snippets/src/core-js/debounce'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  function App() {
+    const [word, setWord] = useState('hello')
+    let info = useFetchDefinition(word)
+    let debouncedSetWord = debounce(setWord, 1000)
+    
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" 
+        name='searchTerm' 
+        id='searchTerm' 
+        className='border border-black'
+        onChange={(e) => debouncedSetWord(e.target.value)}
+        placeholder='Search'
+        />
+        <button className='bg-black text-white'
+        onClick={() => console.log(info)}
+        >Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    )
+
+  }
 
 export default App
