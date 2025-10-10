a964b65518c3721bc36a6c535c25dfa3796d68ff
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-10-10 19:15:29 +0530

    Funny

--- src/components/Definition.jsx
+++ src/components/Definition.jsx
@@ -60,6 +60,7 @@ export default function Definition({ infoProp, searchProp, searchTermProp, loadi
                   </p>
                 )}
               </div>
+
             </div>
           ))
         )}
@@ -67,9 +68,51 @@ export default function Definition({ infoProp, searchProp, searchTermProp, loadi
     );
   }
 
+  if ((searchTermProp === "Triyambika" || searchTermProp === "triyambika") && searchProp){
+    return (
+      <div className="flex justify-center items-center flex-col">
+        <div className="flex items-center justify-center gap-2">
+          <div>
+            <h1 className="text-center font-semibold text-3xl">Triyambika</h1>
+            <div className="border-black border-2 my-4 p-4 w-[920px] max-lg:max-w-[48rem] max-md:max-w-[40rem] max-sm:max-w-[18.75rem] text-black rounded-2xl hover:shadow-xl hover:shadow-black/45 transition-shadow duration-300">
+              <p className="italic">name</p>
+              <div className="ml-4 mb-2 text-left">
+                <p className="font-mono">–Aura ka lauda type shit</p>
+                  <p className="text-gray-600 italic">  
+                    Example: “Triyambika ho gaya”
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  if ((searchTermProp === "Omkar" || searchTermProp === "omkar") && searchProp){
+    return (
+      <div className="flex justify-center items-center flex-col">
+        <div className="flex items-center justify-center gap-2">
+          <div>
+            <h1 className="text-center font-semibold text-3xl">Omkar</h1>
+            <div className="border-black border-2 my-4 p-4 w-[920px] max-lg:max-w-[48rem] max-md:max-w-[40rem] max-sm:max-w-[18.75rem] text-black rounded-2xl hover:shadow-xl hover:shadow-black/45 transition-shadow duration-300">
+              <p className="italic">name</p>
+              <div className="ml-4 mb-2 text-left">
+                <p className="font-mono">-Raging alcoholism manifest</p>
+                  <p className="text-gray-600 italic">  
+                    Example: "Come ra, let's do omkar tonight”
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex justify-center items-center">
       <p className="text-3xl mx-3 text-center">No definitions found for “{searchTermProp}”.</p>
     </div>
-  );
+  )
 }
